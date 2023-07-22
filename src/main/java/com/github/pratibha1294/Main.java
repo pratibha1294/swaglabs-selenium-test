94319342eeab98fef6334476b938507ff9ba7c3b
prathiba <pratibha@healthi.in>
2023-07-22 19:02:40 +0530

    Login & Logout complete

---
+++ src/main/java/com/github/pratibha1294/Main.java
@@ -0,0 +1,13 @@
+package com.github.pratibha1294;
+
+import org.testng.annotations.Test;
+
+public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
+    @Test
+    public void Lallu(){
+        System.out.println("Lallu tera nnada latak ra h");
+    }
+}
