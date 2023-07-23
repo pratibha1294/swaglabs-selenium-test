e7dac3a60e812af674d460fcebe21d0917c49854
prathiba <pratibha@healthi.in>
2023-07-23 18:37:54 +0530

    Created common setup method

--- src/main/java/com/github/pratibha1294/auth/Flow.java
+++ src/main/java/com/github/pratibha1294/auth/Flow.java
@@ -1,24 +1,31 @@
 package com.github.pratibha1294.auth;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import java.util.logging.Logger;
 
 public class Flow {
-@Test
-    public void test_StandardUser() throws InterruptedException {
-        Logger log = Logger.getAnonymousLogger();
+    Logger log;
+    ChromeDriver driver;
+@BeforeSuite
+    private void setUp() {
+        log = Logger.getAnonymousLogger();
         // declaration and instantiation of objects/variables
         System.setProperty("webdriver.chrome.driver", "/home/prathiba/chromedriver_linux64 (1)/chromedriver");
-        ChromeDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+    }
 
+    @Test
+    public void test_StandardUser() throws InterruptedException {
 
 
 // Launch website
-        WebDriver.Navigation ngv= driver.navigate();
+        WebDriver.Navigation ngv = driver.navigate();
         ngv.to("http://saucedemo.com/");
         log.info("Please check navigation");
 //         ngv.wait(8000);
@@ -45,11 +52,40 @@ public class Flow {
         driver.quit();
     }
 
-    public void test_LockedOutUser(){
+    @Test
+    public void test_LockedOutUser() throws InterruptedException {
 
+
+// Launch website
+        WebDriver.Navigation ngv = driver.navigate();
+        ngv.to("http://saucedemo.com/");
+        log.info("Please check navigation");
+//         ngv.wait(8000);
+        WebElement username = driver.findElement(By.id("user-name"));
+        WebElement password = driver.findElement(By.id("password"));
+        username.sendKeys("locked_out_user");
+        password.sendKeys("secret_sauce");
+        driver.findElement(By.name("login-button")).click();
+        log.info("Please check login");
+
+        Thread.sleep(5000);
+        log.info("Please check waiting");
+
+        WebElement hamburgerButton = driver.findElement(By.id("react-burger-menu-btn"));
+        hamburgerButton.click();
+        log.info("Please check menu");
+        Thread.sleep(9000);
+
+        WebElement logoutButton = driver.findElement(By.id("logout_sidebar_link"));
+        logoutButton.click();
+        log.info("Please check logout");
+        Thread.sleep(5000);
+
+        driver.quit();
     }
 
-    public void test_ProblemUser(){
+
+    public void test_ProblemUser() {
 
     }
 }
